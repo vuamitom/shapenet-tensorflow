538a1818231188cf293dd10c78b18603b3ebd5cc
tamvm <tamvm@vng.com.vn>
2019-06-08 10:35:04 +0700

    fix

--- model/shapnet.py
+++ model/shapnet.py
@@ -11,7 +11,9 @@ def shape_layer(shape_mean, components, features):
         features: features extracted from image """    
     batch_size = features.shape[0]
     expanded_components = tf.broadcast_to(components, [batch_size, *components.shape], name='expanded_components')
+    # print('components tensor ', expanded_components)
     features = tf.broadcast_to(features, expanded_components.shape)
+    # print('feature tensor ', features)
     weighted_components = tf.multiply(expanded_components, features, name="weighted_components")
 
     expanded_means = tf.broadcast_to(shape_mean, [batch_size, *shape_mean.shape])
@@ -21,29 +23,44 @@ def shape_layer(shape_mean, components, features):
 def transform_layer(shapes, transform_params):
     indices = dict()
     c = 0
-    for k, v in TRANSFORMS_OPS:
+    for k, v in TRANSFORMS_OPS.items():
         indices[k] = (c, c+v)
         c = c+v
-    print('indices = ', indices)
+    # print('indices = ', indices)
     batch_size = shapes.shape[0]
-
-    scale_params = transform_params[indices['scale'][0]: indices['scale'][1]]
-    rotate_params = transform_params[indices['rotation'][0]: indices['rotation'][1]]
-    translate_params = transform_params[indices['translation'][0]: indices['translation'][1]]
+    
+    transform_params = tf.squeeze(transform_params)# tf.reshape(transform_params, transform_params.shape[0:2])
+    # print('transform_params ', transform_params)
+    scale_params = transform_params[:, indices['scale'][0]: indices['scale'][1]]
+    rotate_params = transform_params[:, indices['rotation'][0]: indices['rotation'][1]]
+    translate_params = transform_params[:, indices['translation'][0]: indices['translation'][1]]
 
     # ensemble transfo
-    trafo_matrix = tf.zeros([N_DIMENS+1, N_DIMENS+1], name='trafo_matrix')
-    trafo_matrix[-1, -1] = 1
-    trafo_matrix = tf.broadcast_to(trafo_matrix, [batch_size, *trafo_matrix.shape])
-    trafo_matrix[:, 0, 0] = tf.multiply(scale_params, tf.cos(rotate_params))[:, 0]
-    trafo_matrix[:, 0, 1] = tf.multiply(scale_params, tf.sin(rotate_params))[:, 0]
-    trafo_matrix[:, 1, 0] = tf.multiply(-scale_params, tf.sin(rotate_params))[:, 0]
-    trafo_matrix[:, 1, 1] = tf.multiply(scale_params, tf.cos(rotate_params))[:, 0]
-    trafo_matrix[:, :-1, -1] = transform_params
+    # trafo_matrix = tf.zeros([N_DIMENS+1, N_DIMENS+1], name='trafo_matrix')
+    # trafo_matrix[-1, -1] = 1
+    # trafo_matrix = tf.broadcast_to(trafo_matrix, [batch_size, *trafo_matrix.shape])
+    # trafo_matrix[:, 0, 0] = tf.multiply(scale_params, tf.cos(rotate_params))[:, 0]
+    # trafo_matrix[:, 0, 1] = tf.multiply(scale_params, tf.sin(rotate_params))[:, 0]
+    # trafo_matrix[:, 1, 0] = tf.multiply(-scale_params, tf.sin(rotate_params))[:, 0]
+    # trafo_matrix[:, 1, 1] = tf.multiply(scale_params, tf.cos(rotate_params))[:, 0]
+    # trafo_matrix[:, :-1, -1] = transform_params
 
+    scale_x_cos = tf.multiply(scale_params, tf.cos(rotate_params))
+    scale_x_sin = tf.multiply(scale_params, tf.sin(rotate_params))
+    neg_scale_x_sin = tf.multiply(-scale_params, tf.sin(rotate_params))
+    # print('scale_x_cos', scale_x_cos)
+    fst_row = tf.concat([scale_x_cos, scale_x_sin], axis=-1)
+    snd_row = tf.concat([neg_scale_x_sin, scale_x_cos], axis=-1)
+    scale_rotate = tf.stack([fst_row, snd_row], axis=-1)
+    trafo_matrix = tf.concat([scale_rotate, tf.reshape(translate_params, [*translate_params.shape, 1])], axis=-1)
+
+    trafo_matrix = tf.concat([trafo_matrix, tf.broadcast_to(tf.constant([0, 0, 1], dtype=tf.float32), [batch_size, 1, 3])], axis=1)
+    # print('trafo_matrix', trafo_matrix)
     # concat 1 more dimen to shape
     shapes = tf.concat([shapes, tf.ones([*shapes.shape[:-1], 1])], -1)
+    # print('shapes = ', shapes)
     transformed_shapes = tf.matmul(shapes, tf.transpose(trafo_matrix, perm=[0, 2, 1]), name='transfo_matmul')
+    # print('transformed_shapes', transformed_shapes)
     return transformed_shapes[:, :, :-1]
 
 def feature_extractor(inputs, num_out_params):
@@ -94,8 +111,8 @@ def feature_extractor(inputs, num_out_params):
 
 def predict_landmarks(inputs, pca_components):
     # shape means are stored at index 0
-    shape_mean = tf.constant(pca_components[0], name='shape_means')
-    components = tf.constant(pca_components[1:], name='components')
+    shape_mean = tf.constant(pca_components[0], name='shape_means', dtype=tf.float32)
+    components = tf.constant(pca_components[1:], name='components', dtype=tf.float32)
 
     in_channels = 1
     n_components = components.shape[0]
@@ -105,15 +122,15 @@ def predict_landmarks(inputs, pca_components):
         n_transforms += v    
 
     num_out_params = n_components + n_transforms  
-    print('num_out_params = ', n_components, n_transforms)
+    # print('num_out_params = ', n_components, n_transforms)
 
     if in_channels == 1:
         inputs = tf.expand_dims(inputs, -1)
 
     features = feature_extractor(inputs, num_out_params)
     features = tf.reshape(features, [inputs.shape[0], num_out_params, 1, 1])
-    print('features shape ', features.shape, features[:, 0:n_components])
+    # print('features shape ', features.shape, features[:, 0:n_components])
     shapes = shape_layer(shape_mean, components, features[:, 0:n_components])
 
-    transformed_shapes = transform_layer(shapes, features[n_components:])
+    transformed_shapes = transform_layer(shapes, features[:, n_components:])
     return transformed_shapes
